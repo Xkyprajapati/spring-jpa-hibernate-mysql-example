931e086a05abf66d99250546a6dadc9356e01fa8
Xkyprajapati <30621434+Xkyprajapati@users.noreply.github.com>
2017-08-17 12:28:10 +0545

    Update UsersResource.java

--- src/main/java/com/techprimers/jpa/springjpahibernateexample/resource/UsersResource.java
+++ src/main/java/com/techprimers/jpa/springjpahibernateexample/resource/UsersResource.java
@@ -4,6 +4,7 @@ import com.techprimers.jpa.springjpahibernateexample.model.Users;
 import com.techprimers.jpa.springjpahibernateexample.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class UsersResource {
         return usersRepository.findOne(id);
     }
 
-    @GetMapping("/update/{id}/{name}")
+    @PutMapping("/update/{id}/{name}")
     public Users update(@PathVariable("id") final Integer id, @PathVariable("name")
                          final String name) {
 
